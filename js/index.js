520f6cba70c33ab4b14637bb4c50c54417655ce1
Denis Kirsanov <excite90@mail.ru>
2017-10-31 16:10:24 +0300

    added smooth scrolling

--- js/index.js
+++ js/index.js
@@ -42,3 +42,14 @@ $('.close-modal').click(function(){
     other.removeClass('active');
 });
 
+//плавный скроллинг
+
+$(document).ready(function(){
+
+		$("#menu").on("click","a", function (event) {
+				event.preventDefault();
+				var id  = $(this).attr('href'),
+						top = $(id).offset().top;
+				$('body,html').animate({scrollTop: top}, 700);
+		});
+});
